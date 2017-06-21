110f1928e7d27a6d1d66581604ab79453cdf2826
Pablo Silva Lopes dos Santos <pablo.santos@smlbrasil.com.br>
2017-06-21 02:17:36 -0300

    NUnit TDD

---
+++ Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Configuration;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Calculadora.Tests
+{
+
+    public class CalculadoraTests
+    {
+
+        [TestFixture]
+        public class CalculadoraSimplesTests
+        {
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Adicionar(5, 5);
+                Assert.That(resultado, Is.EqualTo(10));
+
+            }
+
+
+            [Test]
+            public void DeveMultiplicarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Multiplicar(5, 3);
+                
+                Assert.That(resultado, Is.EqualTo(15));
+            }
+        }
+    }
+}
---
+++ Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+
+        /* Metodo para adicionar dois numeros*/
+
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+        /* Metodo para multiplicar dois numeros*/
+        public int Multiplicar(int num1, int num2)
+        {
+            //Para fins de demo ira gerar um bug
+            return num1 * num2;
+        }
+    }
+}
